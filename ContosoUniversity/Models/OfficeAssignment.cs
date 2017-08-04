107320fa4da5e8ee229d436b070dcd025855afee
Megadragon <leminhthanh0612@gmail.com>
2017-08-04 15:08:20 +0700

    adding officeAssignment

--- ContosoUniversity/Models/OfficeAssignment.cs
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,14 @@ namespace ContosoUniversity.Models
 {
     public class OfficeAssignment
     {
+        [Key]
+        [ForeignKey("Instructor")]
+        public int InstructorID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public virtual Instructor Instructor { get; set; }
     }
 }
