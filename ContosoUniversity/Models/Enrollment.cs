b20a33f7d27f82b35d3ceb2b61de75cd24de2cd5
Megadragon <leminhthanh0612@gmail.com>
2017-08-19 07:16:11 +0700

    add modified view

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -16,6 +17,8 @@ namespace ContosoUniversity.Models
             public int CourseID { get; set; }
 
             public int StudentID { get; set; }
+
+            [DisplayFormat(NullDisplayText = "No grade")]
             public Grade? Grade { get; set; }
             
 
